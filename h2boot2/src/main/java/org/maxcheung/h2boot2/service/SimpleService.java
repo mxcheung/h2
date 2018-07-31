e59e5c3ecbc6c556dbb2952f48ac941b4ed93e71
max cheung <maxcheung@optusnet.com.au>
2018-07-31 23:28:00 +1000

    Environment table

--- h2boot2/src/main/java/org/maxcheung/h2boot2/service/SimpleService.java
+++ h2boot2/src/main/java/org/maxcheung/h2boot2/service/SimpleService.java
@@ -7,7 +7,13 @@ import com.google.common.collect.ImmutableTable;
 
 public interface SimpleService {
 
+	/**
+	 * Return Environment specific properties based on Key Value.
+	 * @param key to retrieve .
+	 * @return property value .
+	 */
 	String getValue(String key);
+	
 	ImmutableTable<String, String, String> processList(List<String> strings);
 	
 }
