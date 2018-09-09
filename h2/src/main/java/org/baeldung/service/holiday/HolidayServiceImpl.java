6a3d6a5029566af52fa639fe3a8620c4b8a8de58
max cheung <maxcheung@optusnet.com.au>
2018-09-09 18:28:28 +1000

    Refactor holiday

--- h2/src/main/java/org/baeldung/service/holiday/HolidayServiceImpl.java
+++ h2/src/main/java/org/baeldung/service/holiday/HolidayServiceImpl.java
@@ -2,12 +2,11 @@ package org.baeldung.service.holiday;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.temporal.TemporalAdjusters;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.baeldung.persistence.model.Calender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +14,8 @@ import org.springframework.stereotype.Service;
 public class HolidayServiceImpl implements HolidayService {
 
 	private final CalenderService calenderService;
-	
+	private final List<DayOfWeek> weekend = Arrays.asList(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
+
 	@Autowired
 	public HolidayServiceImpl(CalenderService calenderService) {
 		super();
@@ -24,28 +24,21 @@ public class HolidayServiceImpl implements HolidayService {
 
 	@Override
 	public List<LocalDate> getNBusinessDays(LocalDate currDate, long size, long daysToAdd) {
-
 		LocalDate start = currDate;
-		LocalDate end = start.plusMonths(1).with(TemporalAdjusters.lastDayOfMonth());
 		List<LocalDate> dates = Stream
 				.iterate(start, date -> date.plusDays(daysToAdd))
-				.filter(d -> (isWorkingDay(d) && !isHoliday(d) ))
+				.filter(d -> (isWorkingDay(d) && !isHoliday(d)))
 				.limit(size)
 				.collect(Collectors.toList());
 		return dates;
 	}
 
 	private boolean isWorkingDay(LocalDate localDate) {
-		DayOfWeek dayOfWeek = localDate.getDayOfWeek();
-		if ((dayOfWeek == DayOfWeek.SATURDAY) || (dayOfWeek == DayOfWeek.SUNDAY)) {
-			return false;
-		}
-		return true;
+		return !weekend.contains(localDate.getDayOfWeek());
 	}
 
 	private boolean isHoliday(LocalDate date) {
-		List<Calender> calenderDates = calenderService.getCalender(date, "exchange");
-		return (!calenderDates.isEmpty());
+		return (!calenderService.getCalender(date, "exchange").isEmpty());
 	}
 
 }
